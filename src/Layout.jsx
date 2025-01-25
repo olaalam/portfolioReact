1646a4efae845f1c74d4b7b7fb2a025bf7d97ec6
olaalam <cds.olaallam382330@alexu.edu.eg>
2025-01-25 17:44:00 +0200

    4th commit

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Navbar from "./Navbar";
-import Footer from "./footer";
+import Footer from "./Footer";
 import { Outlet } from "react-router-dom";
 export default function Layout() {
   return (
